39c56e0855e5334876dd3e3d5e0e115aaac8059f
galvazudys <golfas705@yahoo.com>
2017-09-14 16:48:15 +0100

    working version

---
+++ app.js
@@ -0,0 +1,39 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        myData: '',
+        forkCount:0
+        
+
+    },
+    computed:{
+        getForksCount:function(){
+            return myData.map((item)=>{
+                this.forkCount+=item.forks_count;
+            })
+        }
+    },
+    mounted: function () {
+        return axios
+            .get('https://api.github.com/user/repos?access_token=26b2b54a71e7f8fb16cef469d6b47ec84' +
+                '76288d3')
+            .then(response => {
+                console.log(response)
+                this.myData = response.data;
+                console.log(this.myData)
+            })
+    }
+});
+
+$(document).ready(function() {
+    
+    $(window).scroll(function () {
+
+      if ($(window).scrollTop() > 512) {
+        $('#menu').addClass('navbar-fixed');
+      }
+      if ($(window).scrollTop() < 513) {
+        $('#menu').removeClass('navbar-fixed');
+      }
+    });
+  });
